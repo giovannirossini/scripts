6390d9aaee1a323a235841a30ea0e738c0364d89
Giovanni Rossini <giovanni.rossini@ab-inbev.com>
2022-07-20 18:48:02 -0300

    Adding try/exception

--- python/modules/rabbitmq/connector.py
+++ python/modules/rabbitmq/connector.py
@@ -5,21 +5,31 @@ class RabbitMQ:
   def connect(self, host, port, username, password):
     credentials = pika.PlainCredentials(username, password)
     parameters = pika.ConnectionParameters(host, port, '/', credentials)
-    return pika.BlockingConnection(parameters)
+    try:
+        conn = pika.BlockingConnection(parameters)
+    except Exception as error:
+        print(error)
+    return conn
 
   def create_channel(self, connection):
     self.connection = connection
-    return self.connection.channel()
-
+    try:
+        channel = self.connection.channel()
+    except Exception as error:
+        print(error)
+    return channel
 
   def consume(self, channel, queueName, callback):
     self.channel = channel
     self.queueName = queueName
     self.callback = callback
     self.channel.basic_consume(
-      queue=self.queueName, on_message_callback=self.callback, auto_ack=True
+        queue=self.queueName, on_message_callback=self.callback, auto_ack=True
     )
-    self.channel.start_consuming()
+    try:
+        self.channel.start_consuming()
+    except Exception as error:
+        print(error)
 
   def publish(self, channel, queue, message, headers = None, contentType = None, deliveryMode = None):
     self.channel = channel
@@ -27,22 +37,24 @@ class RabbitMQ:
     self.message = message
     self.headers = headers
     if contentType == None:
-      self.contentType = 'application/octet-stream'
+        self.contentType = 'application/octet-stream'
     else:
-      self.contentType = contentType
+        self.contentType = contentType
     if deliveryMode == None:
-      self.deliveryMode = 1 # 1 - Non-Persistent / 2 - Persistent
+        self.deliveryMode = 1 # 1 - Non-Persistent / 2 - Persistent
     else:
-      self.deliveryMode = deliveryMode
-
-    self.channel.basic_publish(
-      '',
-      self.queue,
-      self.message,
-      pika.BasicProperties(content_type=self.contentType,
-                          delivery_mode=self.deliveryMode,
-                          headers=self.headers
-                          )
-    )
+        self.deliveryMode = deliveryMode
+    try:
+        self.channel.basic_publish(
+            '',
+            self.queue,
+            self.message,
+            pika.BasicProperties(content_type=self.contentType,
+                delivery_mode=self.deliveryMode,
+                headers=self.headers
+            )
+        )
+    except Exception as error:
+        print(error)
 
     return print(f"Message {self.message} published!")
