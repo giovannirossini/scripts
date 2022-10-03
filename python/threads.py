d220a5947efe81bc6976d6138298ad81f6c5310a
Giovanni Rossini <giovannijrrossini@gmail.com>
2022-10-03 10:26:24 -0300

    using threads

---
+++ python/threads.py
@@ -0,0 +1,34 @@
+from time import sleep, perf_counter
+from threading import Thread
+
+def task(id):
+    print(f'Running task {id}...')
+    sleep(0.5)
+    print('Done')
+
+
+print("Starting new tasks...")
+start_time = perf_counter()
+
+for i in range(1,11):
+    task(i)
+
+end_time = perf_counter()
+whithout_thread = f'It took {end_time- start_time: 0.2f} second(s) to complete all tasks without using threads.'
+
+print("Starting new tasks...")
+start_time = perf_counter()
+
+threads = []
+for i in range(1, 11):
+    t = Thread(target=task, args=(i,))
+    threads.append(t)
+    t.start()
+
+# wait for the threads to complete
+for t in threads:
+    t.join()
+
+end_time = perf_counter()
+print(whithout_thread)
+print(f'It took {end_time- start_time: 0.2f} second(s) to complete all tasks using threads.')
