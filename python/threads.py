9fda80a5a779931bcf1edaae361e116073c74bbf
Giovanni Rossini <giovannijrrossini@gmail.com>
2022-10-03 21:00:37 -0300

    if __main__

--- python/threads.py
+++ python/threads.py
@@ -6,29 +6,29 @@ def task(id):
     sleep(0.5)
     print('Done')
 
+if __name__ == '__main__':
+    print('Starting new tasks...')
+    start_time = perf_counter()
 
-print('Starting new tasks...')
-start_time = perf_counter()
+    for i in range(1,11):
+        task(i)
 
-for i in range(1,11):
-    task(i)
+    end_time = perf_counter()
+    whithout_thread = f'It took {end_time- start_time: 0.2f} second(s) to complete all tasks without using threads.'
 
-end_time = perf_counter()
-whithout_thread = f'It took {end_time- start_time: 0.2f} second(s) to complete all tasks without using threads.'
+    print('Starting new tasks...')
+    start_time = perf_counter()
 
-print('Starting new tasks...')
-start_time = perf_counter()
+    threads = []
+    for i in range(1, 11):
+        t = Thread(target=task, args=(i,))
+        threads.append(t)
+        t.start()
 
-threads = []
-for i in range(1, 11):
-    t = Thread(target=task, args=(i,))
-    threads.append(t)
-    t.start()
+    # wait for the threads to complete
+    for t in threads:
+        t.join()
 
-# wait for the threads to complete
-for t in threads:
-    t.join()
-
-end_time = perf_counter()
-print(whithout_thread)
-print(f'It took {end_time- start_time: 0.2f} second(s) to complete all tasks using threads.')
+    end_time = perf_counter()
+    print(whithout_thread)
+    print(f'It took {end_time- start_time: 0.2f} second(s) to complete all tasks using threads.')
